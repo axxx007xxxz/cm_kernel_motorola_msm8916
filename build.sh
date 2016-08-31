15e6afdd79728fc26a7b9d1dfa2f95094a9ee3d0
axxx007xxxz <mattia-sky@hotmail.it>
2016-08-31 16:16:58 +0200

    build.sh: Add help and jobs positional parameters

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
  # Copyright � 2016,  Sultan Qasim Khan <sultanqasim@gmail.com> 		      
  # Copyright � 2016,  Varun Chitre  <varun.chitre15@gmail.com>	
@@ -25,7 +25,18 @@ cyan='\033[0;36m'
 green='\033[0;32m'
 lightblue='\033[1;34m'
 red='\033[0;31m'
-echo "${blue}Setting up${nocol}"
+if [ "$1" = "--help" ]
+		then
+			echo "Usage: ./build.sh -jx"
+			echo "x is number of jobs"
+			exit
+	elif [[ "$1" == "-j"* ]]
+		then
+			jobs="${1}"
+	else
+			jobs="-j4"
+fi
+echo -e "${blue}Setting up${nocol}"
 export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=$(xdg-user-dir)/tools/ubertc-arm-eabi-4.9/bin/arm-eabi-
@@ -34,8 +45,8 @@ export KBUILD_BUILD_HOST="peppermint"
 kernelname="Test"
 kernelversion="1"
 echo
-echo "${blue}Cleaning${nocol}"
-make clean
+echo -e "${blue}Cleaning${nocol}"
+make ${jobs} clean
 rm -f arch/arm/boot/dts/*.dtb
 rm -f arch/arm/boot/dt.img
 rm -fr tmp
@@ -46,22 +57,22 @@ rm -fr flash/system/*
 rm -f ${kernelname}_v${kernelversion}.zip
 echo
 echo
-echo "${lightblue}Compiling ${kernelname} Kernel${nocol}"
+echo -e "${lightblue}Compiling ${kernelname} Kernel${nocol}"
 echo
-echo "${blue}Initializing defconfig${nocol}"
+echo -e "${blue}Initializing defconfig${nocol}"
 make test-lux_defconfig
 echo
-echo "${blue}Building kernel${nocol}"
-make -j4 zImage
-make -j4 dtbs
+echo -e "${blue}Building kernel${nocol}"
+make ${jobs} zImage
+make ${jobs} dtbs
 tools/dtbToolCM -o arch/arm/boot/dt.img -s 2048 -p scripts/dtc/ arch/arm/boot/dts/
 echo
-echo "${blue}Building modules${nocol}"
-make -j4 modules
+echo -e "${blue}Building modules${nocol}"
+make ${jobs} modules
 echo
-echo "${blue}Making flashable zip${nocol}"
+echo -e "${blue}Making flashable zip${nocol}"
 mkdir tmp/modules
-make -j4 modules_install INSTALL_MOD_PATH=tmp/modules INSTALL_MOD_STRIP=1
+make ${jobs} modules_install INSTALL_MOD_PATH=tmp/modules INSTALL_MOD_STRIP=1
 mkdir -p tmp/flash/system/lib/modules/pronto
 find tmp/modules -name '*.ko' -type f -exec cp '{}' tmp/flash/system/lib/modules/ \;
 mv tmp/flash/system/lib/modules/wlan.ko tmp/flash/system/lib/modules/pronto/pronto_wlan.ko
@@ -78,4 +89,4 @@ echo
 echo
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
-echo "$(tput bold)${cyan}Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds!${nocol}$(tput sgr0)"
+echo -e "$(tput bold)${cyan}Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds!${nocol}$(tput sgr0)"
